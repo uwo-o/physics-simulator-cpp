b46bdebea4e9a8a0193de35a8d5b49ded7012838
uwo-o <hugo.campos@usm.cl>
2023-12-09 21:25:54 -0300

    includes folder created

---
+++ includes/constants.h
@@ -0,0 +1,6 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define GRAVITY_ACC 9.81
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <GL/glut.h>
+#include "constants.h"
 
 void init() {
     glClearColor(0.0, 0.0, 0.0, 1.0);
