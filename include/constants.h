51c63f30cb8c54bacbdba083c85a93efac54112e
uwo-o <hugo.campos@usm.cl>
2024-06-18 20:35:42 -0400

    g normalized to fps

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,8 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-static const float GRAVITY = 0.1;
+static const int FRAMES_PER_SECOND = 60;
+static const float GRAVITY = 9.8 * FRAMES_PER_SECOND;
 static const int WINDOW_WIDTH = 800;
 static const int WINDOW_HEIGHT = 600;
 static const float AIR_RESISTANCE = 1.0;
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Physim");
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(FRAMES_PER_SECOND);
 
     ParticleManager particleManager = ParticleManager(&window);
     particleManager.generate_particles(50);
