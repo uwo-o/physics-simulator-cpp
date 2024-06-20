65c7b13b4ce36653bc1493f43821b96ada763d96
uwo-o <hugo.campos@usm.cl>
2024-06-20 16:56:00 -0400

    debugging

--- src/particle_manager.cpp
+++ src/particle_manager.cpp
@@ -8,6 +8,7 @@ ParticleManager::ParticleManager(sf::RenderWindow *window, bool gravity, bool fr
 
 void ParticleManager::grid_search() {
     this->grid.clear();
+
     this->grid = std::vector<std::vector<std::vector<Particle>>>();
     for (int i=0; i<WINDOW_WIDTH; i+=MAX_RADIUS) {
         this->grid.push_back(std::vector<std::vector<Particle>>());
@@ -19,6 +20,7 @@ void ParticleManager::grid_search() {
     for(auto& it : this->particles) {
         int x = it.position.x / MAX_RADIUS;
         int y = it.position.y / MAX_RADIUS;
+
         this->grid[x][y].push_back(it);
         it.update();
     }
@@ -27,7 +29,7 @@ void ParticleManager::grid_search() {
 void ParticleManager::generate_particles(int number) {
     for (int i=0; i<number; i++) {
         Particle p(sf::Vector2f(0, WINDOW_HEIGHT/2), (float) MAX_RADIUS, sf::Color::White);
-        p.velocity = sf::Vector2f(10, 5);
+        p.velocity = sf::Vector2f((float)(rand() % 5 +1), (float)(rand() % 5 +1));
         p.position = sf::Vector2f(0,0);
         if (gravity) p.acceleration = sf::Vector2f(0, GRAVITY);
         if (friction) p.friction = this->friction;
@@ -45,7 +47,7 @@ void ParticleManager::update() {
     for (int i=0; i<this->grid.size(); i++) {
         for (int j=0; j<this->grid[i].size(); j++) {
 
-            auto cell = this->grid[i][j];
+            auto &cell = this->grid[i][j];
 
             if (cell.size() == 0) continue;
 
@@ -54,15 +56,16 @@ void ParticleManager::update() {
 
                     if (i+dx < 0 || i+dx >= this->grid.size() || j+dy < 0 || j+dy >= this->grid[i].size()) continue;
 
-                    auto neighbour = this->grid[i+dx][j+dy];
+                    auto &neighbour = this->grid[i+dx][j+dy];
                     
                     for (auto &it : cell) {
                         for (auto &it2 : neighbour) {
                             if (&it != &it2) {
                                 it.checkCollitionWithParticle(it2);
+                                it.update();
+                                it2.update();
                             }
                         }
-                        it.update();
                     }
                 }
             }
