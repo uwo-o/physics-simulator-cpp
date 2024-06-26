84dbc16c7a9daf1d8cad4766f373a44f47458eed
uwo-o <hugo.campos@usm.cl>
2024-06-26 01:45:36 -0400

    variating parameters

--- include/constants.h
+++ include/constants.h
@@ -5,7 +5,7 @@ static const int FRAMES_PER_SECOND = 60;
 static const float GRAVITY = 9.8f / (float) FRAMES_PER_SECOND;
 static const int WINDOW_WIDTH = 800;
 static const int WINDOW_HEIGHT = 600;
-static const float AIR_RESISTANCE = 1;
-static const float FRICTION = 0.5;
+static const float AIR_RESISTANCE = 0.99f;
+static const float FRICTION = 0.9f;
 
 #endif
